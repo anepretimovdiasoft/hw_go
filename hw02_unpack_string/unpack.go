20c07254cb1ea2fb3330f9d46b4ce70cc3426d83
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2024-09-24 20:26:54 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(srcString string) (string, error) {
+	var builder strings.Builder
+
+	if srcString == "" {
+		return "", nil
+	}
+
+	if isCorrect, err := Verify(srcString); isCorrect && err == nil {
+		runes := []rune(srcString)
+		size := len(runes)
+		for i := 0; i < size-1; i++ {
+			if !unicode.IsDigit(runes[i+1]) && !unicode.IsDigit(runes[i]) {
+				builder.WriteRune(runes[i])
+			}
+			if !unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i+1]) {
+				repeatCount, _ := strconv.Atoi(string(runes[i+1]))
+				if repeatCount != 0 {
+					builder.WriteString(strings.Repeat(string(runes[i]), repeatCount))
+				}
+			}
+		}
+		if !unicode.IsDigit(runes[size-1]) {
+			builder.WriteRune(runes[size-1])
+		}
+	} else {
+		return "", err
+	}
+
+	return builder.String(), nil
+}
+
+func Verify(srcString string) (bool, error) {
+
+	runes := []rune(srcString)
+	if unicode.IsDigit(runes[0]) {
+		return false, ErrInvalidString
+	}
+
+	for i := 1; i < len(runes); i++ {
+		if unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i-1]) {
+			return false, ErrInvalidString
+		}
+	}
+
+	return true, nil
 }
